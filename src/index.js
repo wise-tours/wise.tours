0fa897087526223a90102148e7b2a30300fcefa6
Fi1osof <n.lanets@modxclub.ru>
2019-01-08 22:09:50 +0300

    unregister serviceWorkers

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 
 import PrismaCms from "@prisma-cms/front";
 
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 import App, {
   queryFragments,
@@ -33,5 +33,5 @@ ReactDOM.render(<PrismaCms
   }}
   queryFragments={queryFragments}
 />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
 
