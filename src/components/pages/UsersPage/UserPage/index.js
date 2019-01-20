604a68a814ff7f8916786828ecdea2be64e08e25
Fi1osof <n.lanets@modxclub.ru>
2019-01-20 22:43:16 +0300

    Update UserPage

--- src/components/pages/UsersPage/UserPage/index.js
+++ src/components/pages/UsersPage/UserPage/index.js
@@ -1,129 +1,20 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { graphql, compose } from 'react-apollo';
-// import gql from 'graphql-tag';
-
-import View from './View';
-
-import {
-  // user,
-  updateUserProcessor,
-} from '@modxclub/query';
-
-import Page from '../../layout';
-import gql from 'graphql-tag';
-import PrismaCmsConnector from '@prisma-cms/connector';
-
-import PageNotFound from "../../404";
-
-export class UserPage extends Page {
+import UserPageProto from "@prisma-cms/front/lib/modules/pages/UsersPage/UserPage";
 
 
-  static propTypes = {
-    ...Page.propTypes,
-    View: PropTypes.func.isRequired,
-  }
+import View from "./View";
 
+
+class UserPage extends UserPageProto {
 
   static defaultProps = {
-    ...Page.defaultProps,
+    ...UserPageProto.defaultProps,
     View,
   }
 
-
-
-  setPageMeta(meta = {}) {
-
-    const {
-      data: {
-        object,
-      },
-    } = this.props;
-
-    if (!object) {
-      return;
-    }
-
-    return super.setPageMeta(meta);
-  }
-
-
-  render() {
-
-    const {
-      View,
-      data,
-      ...other
-    } = this.props;
-
-    const {
-      object,
-      loading,
-    } = data;
-
-    if (!object) {
-      if (loading) {
-        return null;
-      }
-      else {
-        return <PageNotFound
-          title="Пользователь не найден"
-        />
-      }
-    }
-
-    return super.render(<View
-      data={data}
-      {...other}
-    />)
-  }
 }
 
 
-export default class UserPageConnector extends PrismaCmsConnector {
-
-  prepareQuery() {
-
-    const {
-      getQueryFragment,
-    } = this.props;
-
-    const UserNoNestingFragment = getQueryFragment("UserNoNestingFragment");
-
-    // console.log("UserNoNestingFragment", UserNoNestingFragment);
-
-
-    const user = gql`
-      query user(
-        $where:UserWhereUniqueInput!
-      ){ 
-        object:user(
-          where:$where
-        ){
-          ...UserNoNesting
-          NotificationTypes{
-            id
-          }
-          EthAccounts{
-            id
-            address
-            balance(
-              convert: ether
-            )
-          }
-        } 
-      }
-
-      ${UserNoNestingFragment}
-    `;
-
-    return compose(
-      graphql(user, {
-      }),
-      graphql(updateUserProcessor, {
-      }),
-    )(UserPage)
-  }
-
-}
+export default UserPage;
