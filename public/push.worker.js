a71859b6b9a28713b3b2f5b8995b53d888a0c017
Fi1osof <n.lanets@modxclub.ru>
2019-02-04 00:13:10 +0300

    Push notifications

--- public/push.worker.js
+++ public/push.worker.js
@@ -8,3 +8,27 @@ const options = {
   badge: 'android-icon-72x72.png'
 };
 self.registration.showNotification(title, options);
+
+self.addEventListener('push', function(event) {
+
+  console.log("addEventListener push event", event);
+
+  let notificationData = {};
+  
+  try {
+    notificationData = event.data.json();
+  } catch (e) {
+    notificationData = {
+      title: 'Default title',
+      body: 'Default message',
+      icon: '/default-icon.png'
+    };
+  }
+  
+  event.waitUntil(
+    self.registration.showNotification(notificationData.title, {
+      body: notificationData.body,
+      icon: notificationData.icon
+    })
+  );
+});
